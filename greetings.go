a7c31721d7e2222930cb022057ae3c2754662ca9
Peskov Sergey <devpeskov@proton.me>
2023-01-17 12:59:39 +0600

    Format with tab-identation. "Go fmt"

--- greetings.go
+++ greetings.go
@@ -1,59 +1,57 @@
 package go_greetings
 
 import (
-  "errors"
-  "fmt"
-  "math/rand"
-  "time"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-  // If no name was given, return an error with a message.
-  if name == "" {
-    return "", errors.New("You wrote an empty name")
-  }
-  // If a name was received, return a value that embeds the name
-  // in a greeting message.
-  message := fmt.Sprintf(randomFormat(), name)
-  return message, nil
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("You wrote an empty name")
+	}
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
 }
 
 // Hellos returns a map of greetings for sequence of named person.
 func Hellos(names []string) (map[string]string, error) {
-  // A map to associate names with messages.
-  messages := make(map[string]string)
-  for _, name := range names {
-    message, err := Hello(name)
-    if err != nil {
-      return nil, err
-    }
-    messages[name] = message
-  }
-  return messages, nil
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
 }
-
 
 // init() executes automatically at programm startup
 // init sets initial values for variables used in the function.
 func init() {
-  rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
-
 
 // randomFormat returns one of a set of greeting messages.
 // The returned message is selected at random.
 func randomFormat() string {
-  // A slice of message formats.
-  formats := []string{
-    "Hi, %v. Welcome!",
-    "Nice to see you, %v!",
-    "Hello, %v!",
-    "Hola amigo, %v!",
-    "Hail, %v. Well met!",
-  }
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Nice to see you, %v!",
+		"Hello, %v!",
+		"Hola amigo, %v!",
+		"Hail, %v. Well met!",
+	}
 
-  // Return a randomly selected message format
-  // by specifying a random index for the slice of the formats.
-  return formats[rand.Intn(len(formats))]
+	// Return a randomly selected message format
+	// by specifying a random index for the slice of the formats.
+	return formats[rand.Intn(len(formats))]
 }
